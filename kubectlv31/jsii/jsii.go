c6e819f76f9c9ea750f7a684e3ffd66d6d99bd2b
AWS CDK Team <aws-cdk@amazon.com>
2025-05-16 19:04:43 +0000

    chore(release): v2.1.0

--- kubectlv31/jsii/jsii.go
+++ kubectlv31/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed aws-cdk-lambda-layer-kubectl-v31-2.0.3.tgz
+//go:embed aws-cdk-lambda-layer-kubectl-v31-2.1.0.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@aws-cdk/lambda-layer-kubectl-v31", "2.0.3", tarball)
+	_jsii_.Load("@aws-cdk/lambda-layer-kubectl-v31", "2.1.0", tarball)
 }
